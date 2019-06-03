03e3712f217823c052c7c26660de1845b3a7957f
MAHANTESH GHULAPPANAVAR <41230677+MahanteshNG@users.noreply.github.com>
2019-06-03 18:14:59 +0530

    Update pyramid.c

--- pyramid.c
+++ pyramid.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+void main()
+{
+    int i, j, k=0;
+  printf("Enter the number of rows");
+  scanf(%s",&a);
+    for(i=1; i<=a; i++)
+    {
+	for(j=1; j<=(a-i); j++)
+	{
+	   printf(" ");
+	}
+	while(k!=(2*i-1))
+	{
+	   printf("*");
+	   k++;
+	}
+	k=0;
+	printf("\n");
+    }
+}
